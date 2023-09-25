b80b5089ada61e889e95423ab1fda688e84524e7
Michael Kline <klinem@gmail.com>
2023-09-25 03:32:07 -0700

    small changes for deployment

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const cookieParser = require("cookie-parser");
 
 app.use(
   cors({
-    origin: ["http://localhost:3000"],
+    origin: ["http://localhost:3000 || https://mkline.org/Recipe_app"],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,    
   })
